f55341b3a6e317f9e26a41d6c7fa6859ff090319
Marc Trudelle <82558285+marcduck@users.noreply.github.com>
2023-04-11 02:08:12 -0400

    Fix svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar({ bidderId }) {
       justify-between mx-auto p-4"
       >
         <Link to={"/"} className="flex items-center">
-          <motion.img
+          <motion.svg
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.95 }}
             transition={{
@@ -41,10 +41,21 @@ export default function Navbar({ bidderId }) {
               type: "spring",
               bounce: 0.7,
             }}
-            src="./HomeModern.svg"
             className="h-8 mr-3"
-            alt={appName + " Logo"}
-          />
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+            ></path>
+          </motion.svg>
+
           <motion.span
             whileDrag={{ scale: 0.98 }}
             whileHover={{ scale: 1.01 }}
