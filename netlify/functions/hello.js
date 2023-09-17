d353c6774b016cf4e279f61c5840ec6dc4f558e4
Marc Trudelle <82558285+marcduck@users.noreply.github.com>
2023-09-16 21:31:29 -0400

    Fix message name

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = async (event, context) => {
   const data = { message: "Hello from Netlify Functions!" };
   return {
     statusCode: 200,
-    body: JSON.stringify(message),
+    body: JSON.stringify(data.message),
   };
 };
