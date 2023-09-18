57ecadda36ec0ec418ecdcadb027611fc4462e9e
Marc Trudelle <82558285+marcduck@users.noreply.github.com>
2023-09-17 22:15:36 -0400

    Rename domain

--- netlify/functions/fetchProperty.cjs
+++ netlify/functions/fetchProperty.cjs
@@ -10,17 +10,16 @@ const sanity = createClient({
 });
 
 exports.handler = async (event) => {
-  try {
-    // Parse the incoming POST request body as JSON
-    // const requestBody = JSON.parse(event.body);
+  // Parse the incoming POST request body as JSON
+  // const requestBody = JSON.parse(event.body);
 
-    // Extract the _id from the request body
-    // const _id = "01ebe080-a3ed-457a-81c4-62847f0eb1f8";
-    console.log(JSON.parse(event.body));
-    const { _id } = JSON.parse(event.body);
+  // Extract the _id from the request body
+  // const _id = "01ebe080-a3ed-457a-81c4-62847f0eb1f8";
+  console.log(JSON.parse(event.body));
+  const { _id } = JSON.parse(event.body);
 
-    // Construct a query to fetch the specific post based on _id
-    const query = `*[_type == "gallery" && _id == $postId] {
+  // Construct a query to fetch the specific post based on _id
+  const query = `*[_type == "gallery" && _id == $postId] {
       _id,
       title,
       image,
@@ -29,45 +28,25 @@ exports.handler = async (event) => {
       highestBidder
     }`;
 
-    const properties = await sanity
-      .fetch(query, { postId: _id })
-      .then((data) => {
-        data = data[0];
-        const builder = imageUrlBuilder(sanity);
-
-        return {
-          ...data,
-          // Formatted image URL
-          propertyImage: builder
-            .image(data.image)
-            .fit("max")
-            .auto("format")
-            .url(),
-        };
-      });
+  const properties = await sanity.fetch(query, { postId: _id }).then((data) => {
+    data = data[0];
+    const builder = imageUrlBuilder(sanity);
 
     return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(properties),
+      ...data,
+      // Formatted image URL
+      propertyImage: builder.image(data.image).fit("max").auto("format").url(),
     };
-  } catch (error) {
-    console.error("Error fetching post:", error);
+  });
 
-    return {
-      statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ error: "Internal Server Error" }),
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(properties),
+  };
 };
