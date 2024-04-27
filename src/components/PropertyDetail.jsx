4ea3f0cc50d783d55a2c35d54f3c47cad6a33805
Marc Trudelle <82558285+marcduck@users.noreply.github.com>
2024-04-26 21:01:07 -0400

    Remove MUI icons

--- src/components/PropertyDetail.jsx
+++ src/components/PropertyDetail.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Property } from "./Property";
 import { Link, useParams } from "react-router-dom";
 import { fetchData, fetchPostById } from "../utils";
-import ArrowBack from "@mui/icons-material";
 
 export default function PropertyDetail({ bidderId, balance, setBalance }) {
   let { id } = useParams();
@@ -27,7 +26,6 @@ export default function PropertyDetail({ bidderId, balance, setBalance }) {
     <div className="relative page-container flex flex-col items-center">
       <Link to="/properties" className="">
         <button className="md:absolute left-0  hover:border-emerald-500/50 border-transparent border-solid border-2 rounded-md flex py-1 px-2 items-center gap-2">
-          <ArrowBack fontSize="1rem" />
           Back
         </button>
       </Link>
